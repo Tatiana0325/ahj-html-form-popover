17aa9ae36b30e3eff71bebe0bf99d9528195974f
Tatiana Krasnova <krasnova-tatyana92@mail.ru>
2020-09-26 22:45:06 +0300

    Changing

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,29 +1,42 @@
 import puppetteer from 'puppeteer';
+import { fork } from 'child_process';
 
 jest.setTimeout(30000); // default puppeteer timeout
 
-describe('Popover', () => {
+describe('Popover validete', () => {
   let browser = null;
   let page = null;
+  let server = null;
   const baseUrl = 'http://localhost:9000';
 
   beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on('error', reject);
+      server.on('message', (message) => {
+        if (message === 'ok') {
+          resolve();
+        }
+      });
+    });
+
     browser = await puppetteer.launch({
-      headless: false,
-      slowMo: 250,
-      devtools: true,
+      // headless: false, // show gui
+      // slowMo: 250,
+      // devtools: true, // show devTools
     });
     page = await browser.newPage();
   });
 
   afterAll(async () => {
     await browser.close();
-  });
+    server.kill();
+	});
   
   test('should add .active class', async () => {
     await page.goto(baseUrl);
     const button = await page.$('[data-toggle="button-popover"]');
     button.click();
-    await page.waitForSelector('[data-widget="popover-top"].message-popover');
+    await page.waitForSelector('[data-widget="popover"].active');
   });
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,21 @@
 const formPop = document.querySelector('.form-popover');
-const message = formPop.querySelector('.message-popover');
 const btnPop = formPop.querySelector('.button-popover');
 
 btnPop.addEventListener('click', (event) => {
 	event.preventDefault();
 
-	message.classList.remove('invisible');
-	message.classList.add('active');
+	formPop.classList.add('active')
 
-	message.style.left = `${message.offsetLeft + (btnPop.offsetWidth - message.offsetWidth) / 2}px`;
+	const message = document.createElement('div');
+	message.setAttribute('data-widget', 'popover');
+	message.classList.add('message-popover');
+	message.innerHTML = `
+		<div class="popover-title">Popover title</div>
+		<div class="popover-body">And here's some amazing content. It's very engaging. Right?</div>
+	`;
+
+	btnPop.appendChild(message);
+	message.classList.add('active');
+	message.style.left = `${btnPop.offsetWidth  + message.offsetWidth / 2 - (message.offsetLeft - btnPop.offsetLeft) / 2}px`;
 	message.style.top = `${btnPop.offsetTop - (message.offsetHeight + 5)}px`
 })
